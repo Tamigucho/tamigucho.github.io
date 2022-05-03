a56b80aa10edf7631fb24aa7106072250d6306e4
Daniella Mesquita <daniellmesquita@protonmail.com>
2022-05-02 21:05:50 -0300

    Init <3

---
+++ js/page-common.js
@@ -0,0 +1,18 @@
+/*
+	Framework.js r2 | (c) 2014 - 2019 Plasnerd | http://framework.js.org/LICENSE.md
+*/
+
+var body = document.getElementsByTagName("BODY")[0];
+
+    // Depending on user theme settings, set the theme
+//    switch(server_info.user_settings.theme) {
+//    case "light":
+//        body.innerHTML += ''
+//        break
+//    case "dark":
+//        body.innerHTML += '<link rel="stylesheet" type="text/css" href="css/dark-common.css"><link rel="stylesheet" type="text/css" href="css/dark-custom.css">'
+//        break
+//    default:
+//        console.log("Theming not supported. Please upgrade your ZeroNet version.")
+//    }
+//})
---
+++ js/page-custom.js
@@ -0,0 +1,4 @@
+/*Clones the zite you want. You can use this as model to create your own clonning functions*/
+function cloneselect() {
+return zeroPage.cmd("siteClone", ["1Fmwk685eaX7GjoQ7JKo3QvywizGfnYRCB"]);
+}
