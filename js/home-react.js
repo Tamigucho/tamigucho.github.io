b70dd1b207294c288edb090c4d21b567d2ca8eee
Daniella Mesquita <mesquitadaniella@proton.me>
2026-02-20 06:17:37 -0300

    Add React slideshow with countdown and mobile-first tweaks

--- js/home-react.js
+++ js/home-react.js
@@ -22,8 +22,19 @@
     );
   }
 
+  function SlideMarker(props) {
+    return e('button', {
+      className: 'slide-marker' + (props.active ? ' active' : ''),
+      onClick: props.onClick,
+      'aria-label': 'Go to slide ' + (props.index + 1)
+    }, e('span', null, String(props.index + 1).padStart(2, '0')));
+  }
+
   function HomeApp() {
     var _a = useState(0), scrollY = _a[0], setScrollY = _a[1];
+    var _b = useState(0), activeSlide = _b[0], setActiveSlide = _b[1];
+    var slideDuration = 6;
+    var _c = useState(slideDuration), secondsLeft = _c[0], setSecondsLeft = _c[1];
 
     useEffect(function () {
       var ticking = false;
@@ -47,6 +58,26 @@
       ];
     }, []);
 
+    var slides = useMemo(function () {
+      return [
+        {
+          title: 'Rise of Tamigucho multiverse',
+          text: 'Do lore clássico até hubs experimentais. Uma franquia que trata criaturas como cultura pop viva.',
+          image: 'https://assets.tamigucho.com/assets/cms2/img/tamipedia/full/001.png'
+        },
+        {
+          title: 'DAO energy sem estética preguiçosa',
+          text: 'Web3 só vale quando vira utilidade. Aqui o visual puxa a comunidade pra participar de verdade.',
+          image: './img/5d30129fbe8118efde2a8a25_Badge Dark.svg'
+        },
+        {
+          title: 'Cards, collabs e timeline viva',
+          text: 'Cada seção abre novas entradas no universo. Menos landing morta, mais mundo navegável.',
+          image: './tamipedia/img/IMG_20220629_185301_e~2.png'
+        }
+      ];
+    }, []);
+
     var regions = [
       { name: 'Tamipedia', text: 'Lore, criaturas, tipos e tudo que expande o universo.', href: './tamipedia' },
       { name: 'Cards', text: 'Coleções e artes oficiais pra quem curte a estética moncatching.', href: './cards' },
@@ -54,10 +85,61 @@
       { name: 'Sunny & Rainny', text: 'Edição climática com identidade forte e contraste visual.', href: 'https://sunnyrainny.tamigucho.com/', external: true }
     ];
 
+    useEffect(function () {
+      // Slideshow timer: counts down each second and loops to next slide.
+      var id = window.setInterval(function () {
+        setSecondsLeft(function (current) {
+          if (current <= 1) {
+            setActiveSlide(function (prev) {
+              return (prev + 1) % slides.length;
+            });
+            return slideDuration;
+          }
+          return current - 1;
+        });
+      }, 1000);
+
+      return function () {
+        window.clearInterval(id);
+      };
+    }, [slides.length]);
+
+    function goToSlide(index) {
+      setActiveSlide(index);
+      setSecondsLeft(slideDuration);
+    }
+
+    var currentSlide = slides[activeSlide];
+
     return e('section', { className: 'anime-react-home' },
       e(FloatingOrb, { className: 'orb-a', offset: scrollY * -0.08 }),
       e(FloatingOrb, { className: 'orb-b', offset: scrollY * -0.12 }),
       e(FloatingOrb, { className: 'orb-c', offset: scrollY * -0.05 }),
+
+      e('section', { className: 'top-slideshow' },
+        e('div', { className: 'slideshow-main' },
+          e('div', { className: 'slideshow-copy' },
+            e('p', { className: 'hero-tag' }, 'Featured storyline sequence'),
+            e('h2', null, currentSlide.title),
+            e('p', null, currentSlide.text),
+            e('div', { className: 'slide-countdown' },
+              e('strong', null, secondsLeft + 's'),
+              e('span', null, ' para próxima transição')
+            )
+          ),
+          e('div', { className: 'slideshow-media' },
+            e('img', { src: currentSlide.image, alt: currentSlide.title })
+          )
+        ),
+        e('div', { className: 'slide-markers' }, slides.map(function (_, index) {
+          return e(SlideMarker, {
+            key: 'marker-' + index,
+            index: index,
+            active: index === activeSlide,
+            onClick: function () { goToSlide(index); }
+          });
+        }))
+      ),
 
       e('div', { className: 'hero-landscape' },
         e('div', { className: 'hero-copy', style: { transform: 'translateY(' + (scrollY * -0.03) + 'px)' } },
